e8e31863358b344d4efc28375d771c391c04a46c
MaidSafe-QA <qa@maidsafe.net>
2016-02-03 13:36:48 +0000

    Updated documentation.

---
+++ master/implementors/rustc_serialize/serialize/trait.Decodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['socket_addr'] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Decodable.html' title='rustc_serialize::serialize::Decodable'>Decodable</a> for <a class='struct' href='socket_addr/struct.SocketAddr.html' title='socket_addr::SocketAddr'>SocketAddr</a>","impl <a class='trait' href='rustc_serialize/serialize/trait.Decodable.html' title='rustc_serialize::serialize::Decodable'>Decodable</a> for <a class='struct' href='socket_addr/struct.SocketAddrV4.html' title='socket_addr::SocketAddrV4'>SocketAddrV4</a>","impl <a class='trait' href='rustc_serialize/serialize/trait.Decodable.html' title='rustc_serialize::serialize::Decodable'>Decodable</a> for <a class='struct' href='socket_addr/struct.SocketAddrV6.html' title='socket_addr::SocketAddrV6'>SocketAddrV6</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
